b7bc3b33a1c57f1f3aeaabd0c0f9238441642d26
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-07-12 22:50:35 +0100

    Stop game sprinting if a player leaves their bed

--- src/main/java/cloud/drakon/ticksleep/TickSleep.java
+++ src/main/java/cloud/drakon/ticksleep/TickSleep.java
@@ -4,11 +4,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.ServerTickManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.world.TimeSkipEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TickSleep extends JavaPlugin implements Listener {
     private final ServerTickManager serverTickManager = Bukkit.getServerTickManager();
+    private boolean sprinting = false;
 
     @Override
     public void onEnable() {
@@ -25,6 +27,17 @@ public class TickSleep extends JavaPlugin implements Listener {
             // Initiate a sprint for the same amount of ticks as would've been skipped
             // TODO: Get if a sprint was already initiated and if it was for longer than our requested sprint, and do nothing if it is
             serverTickManager.requestGameToSprint((int) event.getSkipAmount());
+            sprinting = true;
+        }
+    }
+
+    @EventHandler
+    public void onPlayerBedLeave(PlayerBedLeaveEvent event) {
+        // If a sprint is already initiated because of us
+        if (sprinting) {
+            // Stop the sprint
+            serverTickManager.stopSprinting();
+            sprinting = false;
         }
     }
 }
