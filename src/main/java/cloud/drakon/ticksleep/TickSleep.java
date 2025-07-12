f2ec8a5d60b45c2f67a0f894a29b4681607e0142
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-07-12 21:44:29 +0100

    Request game to sprint instead of time skipping

--- src/main/java/cloud/drakon/ticksleep/TickSleep.java
+++ src/main/java/cloud/drakon/ticksleep/TickSleep.java
@@ -1,12 +1,15 @@
 package cloud.drakon.ticksleep;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ServerTickManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.TimeSkipEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TickSleep extends JavaPlugin implements Listener {
+    private final ServerTickManager serverTickManager = Bukkit.getServerTickManager();
+
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
@@ -16,6 +19,8 @@ public class TickSleep extends JavaPlugin implements Listener {
     public void onTimeSkip(TimeSkipEvent event) {
         if (event.getSkipReason() == TimeSkipEvent.SkipReason.NIGHT_SKIP) {
             event.setCancelled(true);
+
+            serverTickManager.requestGameToSprint((int) event.getSkipAmount());
         }
     }
 }
