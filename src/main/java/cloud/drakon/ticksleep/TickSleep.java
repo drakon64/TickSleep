5c621a612a05513ad3bcc5f68e7840d09cb1d8f6
Adam Chance <6444703+drakon64@users.noreply.github.com>
2025-07-12 22:49:16 +0100

    Comments

--- src/main/java/cloud/drakon/ticksleep/TickSleep.java
+++ src/main/java/cloud/drakon/ticksleep/TickSleep.java
@@ -17,9 +17,13 @@ public class TickSleep extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onTimeSkip(TimeSkipEvent event) {
+        // If the time skip is initiated by players sleeping in beds
         if (event.getSkipReason() == TimeSkipEvent.SkipReason.NIGHT_SKIP) {
+            // Cancel the time skip
             event.setCancelled(true);
 
+            // Initiate a sprint for the same amount of ticks as would've been skipped
+            // TODO: Get if a sprint was already initiated and if it was for longer than our requested sprint, and do nothing if it is
             serverTickManager.requestGameToSprint((int) event.getSkipAmount());
         }
     }
